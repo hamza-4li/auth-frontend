ffd5b738bf57b4937710a55d7506ad2284642e7b
hamza-4li <sy.hamza.4li@gmail.com>
2025-02-10 15:35:12 +0500

    Url changed

--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -11,7 +11,7 @@ export default function Signup() {
     const handleSignup = async (e) => {
         e.preventDefault();
         try {
-            const res = await fetch('http://localhost:5000/register', {
+            const res = await fetch('auth-backend-production-20bb.up.railway.app/register', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ email, password }),
