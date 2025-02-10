6fbe55a9bef46200762adc96c52755bbe36d6a5b
hamza-4li <sy.hamza.4li@gmail.com>
2025-02-10 16:15:23 +0500

    reference changed

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -11,7 +11,7 @@ export default function Login() {
     const handleLogin = async (e) => {
         e.preventDefault();
         try {
-            const res = await fetch('auth-backend-production-20bb.up.railway.app/login', {
+            const res = await fetch('https://auth-backend-production-20bb.up.railway.app/login', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ email, password }),
--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -11,7 +11,7 @@ export default function Signup() {
     const handleSignup = async (e) => {
         e.preventDefault();
         try {
-            const res = await fetch('auth-backend-production-20bb.up.railway.app/register', {
+            const res = await fetch('https://auth-backend-production-20bb.up.railway.app/register', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ email, password }),
